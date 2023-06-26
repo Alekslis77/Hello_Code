382dfb482427a33f034db7421e025622b58e1c96
Алексей Листов <ailistov77@yandex.ru>
2023-06-26 09:37:07 +0300

    add Task00_Квадрат числа

---
+++ Task00_квадрат числа/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Напишите программу, которая навход принимает чмслоивыдает его квадрат
+(чмсло умноженное на самого себя).
+
+4 -> 16
+-3 -> 9
+-7 -> 49
+*/
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number * number;
+Console.WriteLine(result);
